265611570754cce53e54e3e3260f1ba0b1b3a75c
kmitz-ftp <kmitz@free.fr>
2015-07-13 18:40:24 +0200

    Add MaxTemp
    The temperature taken into account for regulation if max (temp1,temp2)

--- regul.sh
+++ regul.sh
@@ -7,22 +7,24 @@ while true
 do
 	date
 	echo TEMP REGULATION IS ACTIVE
-	temp=$(cgminer-api stats | grep temp1] | awk '{print $3}') 
-	echo Temperature: $temp
+	temp1=$(cgminer-api stats | grep temp1] | awk '{print $3}') 
+	temp2=$(cgminer-api stats | grep temp2] | awk '{print $3}')
+	maxtemp=$((	$temp1 > $temp2 ? $temp1 : $temp2 ))
+	echo Temperature: $maxtemp
 	freq=$(cgminer-api stats | grep frequency] | awk '{print $3}')
 	echo Frequence: $freq
-	if [ $temp -gt $TMAX ]
+	if [ $maxtemp -gt $TMAX ]
 	then 
-		echo "Aaahhhhhh!! I'm burning!! $temp Celsius degrees";
+		echo "Aaahhhhhh!! I'm burning!! $maxtemp Celsius degrees";
 		newFreq=$(cat freqList | grep -A "$FREQ_STEP" "$freq" | tail -n 1)
 		echo New Frequency: $newFreq	
 	        sed -i "/bitmain-freq/c\"bitmain-freq\" : \"$newFreq\"," /config/cgminer.conf;    
 		echo Restarting...
 		sleep 1s
 		/etc/init.d/cgminer.sh restart
-	elif [ $temp -lt $TMIN ]
+	elif [ $maxtemp -lt $TMIN ]
 	then
-		echo "Increase the power...!! Only $temp Celsius degrees"
+		echo "Increase the power...!! Only $maxtemp Celsius degrees"
 		newFreq=$(cat freqList | grep -B "$FREQ_STEP" "$freq" | head -n 1)
 		echo New Frequency: $newFreq
 		sed -i "/bitmain-freq/c\"bitmain-freq\" : \"$newFreq\"," /config/cgminer.conf;
@@ -30,7 +32,7 @@ do
 		sleep 1s
 		/etc/init.d/cgminer.sh restart
 	else
-		echo "Temp OK: $temp Celsius degrees";
+		echo "Temp OK: $maxtemp Celsius degrees";
 	fi
 
 	sleep $REFRESH_TIMER
